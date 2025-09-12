3474aafcbe6b628e0719cc10fb34086be2ef057d
Muscab Hassan <Muscab.hassan@mnsu.edu>
2025-09-12 16:50:55 -0500

    Add files via upload

---
+++ Search_Insertion.py
@@ -0,0 +1,17 @@
+def SearchInsert(array, target):
+
+    left, right = 0, len(array)-1
+
+    while left <= right:
+        Mid = (left + right) // 2
+
+        if array[Mid] == target:
+            return Mid
+        elif array[Mid] < target:
+            left = Mid + 1
+        else:
+            right = Mid -1
+
+    return left
+
+print(SearchInsert(array=[-2, 0, 3, 5, 6, 9, 11], target= 7))
